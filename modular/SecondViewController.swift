7f110994034c1e6179a7baf9b66f78578430811f
Arlo Armstrong <arlo@goodeggs.com>
2016-10-07 14:53:13 -0700

    Initial commit

---
+++ modular/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  modular
+//
+//  Created by Arlo Armstrong on 10/7/16.
+//  Copyright © 2016 Arlo Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
